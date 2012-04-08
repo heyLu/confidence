97d2d18590d9e5f8cced7d98cfc133c1c7170feb
Lucas Stadler <mi.au@papill0n.org>
2012-04-08 17:33:15 +0200

    ~/.config/awesome/rc.lua: Always put pidgin/chromium on the right tags.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -338,9 +338,12 @@ awful.rules.rules = {
       properties = { floating = true } },
     { rule = { class = "gimp" },
       properties = { floating = true } },
-    -- Set Firefox to always map on tags number 2 of screen 1.
-    -- { rule = { class = "Firefox" },
-    --   properties = { tag = tags[1][2] } },
+
+    -- my own rules
+    { rule = { class = "Chromium" },
+      properties = { tag = tags[1][2] } },
+    { rule = { class = "Pidgin" },
+      properties = { tag = tags[1][3] } }
 }
 -- }}}
 
