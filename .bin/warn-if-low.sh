9ec8e8564970abf38f6e806437396091b53e067e
Lucas Stadler <mi.au@papill0n.org>
2013-04-24 11:48:41 +0200

    ~/.bin/warn-if-low.sh: Support for my new laptop.
    I think support for the old one is broken, but for now I don't use it.

--- .bin/warn-if-low.sh
+++ .bin/warn-if-low.sh
@@ -3,11 +3,11 @@
 # Warn if the battery status is lower than 10%
 
 charge_path="/sys/class/power_supply"
-charge_full=`cat $charge_path/BAT0/charge_full`
-charge_now=`cat $charge_path/BAT0/charge_now`
+charge_full=`cat $charge_path/BAT0/energy_full`
+charge_now=`cat $charge_path/BAT0/energy_now`
 charge_rate=$(($charge_now * 100 / $charge_full ))
 
-online=`cat $charge_path/AC0/online`
+online=`cat $charge_path/AC/online`
 
 if [ "$online" = "0" -a $charge_rate -lt 10 ]; then
 	DISPLAY=:0 zenity --error --text "Whoopey: You're low on energy (only $charge_rate% left)!"
