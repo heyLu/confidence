200d6e4ff974520f2d289133cdd807792dc42c05
Lucas Stadler <mi.au@papill0n.org>
2013-06-16 17:50:09 +0200

    ~/.xmonad: Remove old fullscreen hook.
    Now, why does stalonetray ignore fullscreen?

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -59,33 +59,3 @@ main = do
           let switchLang cur = if cur == "us" then "de" else "us"
           spawn $ "setxkbmap " ++ switchLang kb
           modifyIORef kbMap switchLang
-
-evHook :: Event -> X All
-evHook (ClientMessageEvent _ _ _ dpy win typ dat) = do
-    state <- getAtom "_NET_WM_STATE"
-    fullsc <- getAtom "_NET_WM_STATE_FULLSCREEN"
-    isFull <- runQuery isFullscreen win
-
-    let remove = 0
-        add = 1
-        toggle = 2
-
-        ptype = 4
-
-        action = head dat
-
-    when (typ == state && (fromIntegral fullsc) `elem` tail dat) $ do
-        when (action == add || (action == toggle && not isFull)) $ do
-            io $ changeProperty32 dpy win state ptype propModeReplace [fromIntegral fullsc]
-            fullFloat win
-        when (head dat == remove || (action == toggle && isFull)) $ do
-            io $ changeProperty32 dpy win state ptype propModeReplace []
-            tileWin win
-
-    return $ All False
-
-evHook _ = return $ All True
-
-fullFloat, tileWin :: Window -> X () 
-fullFloat w = windows $ W.float w (W.RationalRect 0 0 1 1)
-tileWin   w = windows $ W.sink w
