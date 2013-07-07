1df48980ff4ed8b2cdf3035e9667a8259921523b
Lucas Stadler <mi.au@papill0n.org>
2013-07-07 12:47:31 +0200

    ~/.config/taffybar: Add memory usage widget.
    Surprisingly, only calling pollingLabelNew doesn't do the job, we have
    to call Graphics.UI.Gtk.widgetShowAll, too.

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -4,11 +4,22 @@ import System.Taffybar.Systray
 import System.Taffybar.XMonadLog
 import System.Taffybar.SimpleClock
 import System.Taffybar.Widgets.PollingGraph
+import System.Taffybar.Widgets.PollingLabel
 import System.Information.CPU
+import System.Information.Memory (parseMeminfo, memoryUsedRatio)
+import Graphics.UI.Gtk (widgetShowAll)
 
 cpuCallback = do
     (_, systemLoad, totalLoad) <- cpuLoad
     return [ totalLoad, systemLoad ]
+
+formatMeminfo m = "Mem: " ++ memPercentage ++ "%"
+    where memPercentage = show . truncate . ((*) 100) $ memoryUsedRatio m
+
+textMemoryNew intervalSeconds = do
+    memLabel <- pollingLabelNew "" intervalSeconds (parseMeminfo >>= return . formatMeminfo)
+    widgetShowAll memLabel
+    return memLabel
 
 main = do
     let cpuCfg = defaultGraphConfig {
@@ -18,9 +29,10 @@ main = do
         log = xmonadLogNew
         tray = systrayNew
         cpu = pollingGraphNew cpuCfg 3 cpuCallback
-        battery = textBatteryNew "%d%%" 30
+        battery = textBatteryNew "Bat: %d%%" 30
+        memory = textMemoryNew 3
     defaultTaffybar defaultTaffybarConfig {
         barHeight = 15,
         startWidgets = [log],
-        endWidgets = [tray, clock, battery, cpu]
+        endWidgets = [tray, clock, memory, battery, cpu]
     }
