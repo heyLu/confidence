aa4e4031a725adb1b0ca1bd13524c902d33dd8a9
Lucas Stadler <lu@papill0n.org>
2014-12-21 14:23:46 +0100

    ~/.emacs.d: make the initial buffer a text buffer
    e.g. (try to) write more.
    
    thanks, internet: http://emacsredux.com/blog/2014/07/25/configure-the-scratch-buffers-mode/

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,6 +1,9 @@
 ; Mhh, let's see where this goes. (I hope I don't grow a beard.)
 (load "~/.emacs.d/better-defaults")
 
+(setq initial-major-mode 'text-mode)
+(setq initial-scratch-message "<3")
+
 (setq auto-save-file-name-transforms
       `((".*" ,(concat user-emacs-directory "backups"))))
 
