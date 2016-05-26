7438c769ded6c8e4c7f7ef1a41ceb10e9ba88bdb
Lucas Stadler <lu@papill0n.org>
2016-05-26 16:45:25 +0200

    ~/.bin/check-papill0n.sh: Check if services are up
    This is just a very basic check, but really useful for quick checks, as
    there will be errors when a proxied service isn't running, the entire
    server is down, ...

---
+++ .bin/check-papill0n.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+status=0
+
+check_url() {
+	url="$1"
+
+	if curl --fail --show-error --silent --location "$url" > /dev/null; then
+		echo -e "[\033[0;32m✓\033[0m] $url"
+	else
+		echo -e "[ ] \033[0;31m$url\033[0m"
+		status=1
+	fi
+}
+
+check_url papill0n.org
+check_url papill0n.org/favicon?url=https://google.com
+check_url git.papill0n.org
+check_url one.papill0n.org
+check_url pixl.papill0n.org
+check_url pad.papill0n.org
+
+check_url heartheartheart.club
+
+exit $status
