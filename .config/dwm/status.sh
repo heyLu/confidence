dde619a4d7b55da030f8ba15ccda992c6e79f34c
Lucas Stadler <lu@papill0n.org>
2017-01-09 17:18:59 +0100

    ~/.config/dwm/status.sh: Use icons in status bar
    Including one that indicates whether the laptop is currently plugged in.

--- .config/dwm/status.sh
+++ .config/dwm/status.sh
@@ -7,9 +7,13 @@ charge_path="/sys/class/power_supply"
 charge_full=`cat $charge_path/BAT0/energy_full`
 charge_now=`cat $charge_path/BAT0/energy_now`
 charge_rate=$(($charge_now * 100 / $charge_full ))
-status="$status$charge_rate%"
+charge_icon=" "
+if [ "$(cat $charge_path/AC/online)" -eq "1" ]; then
+	charge_icon="⚡"
+fi
+status="$status   $charge_icon $charge_rate%"
 
 # date
-status="$status   $(date '+%a %b %_d %H:%M')"
+status="$status   🕐 $(date '+%a %b %_d %H:%M')"
 
 echo "$status"
