3601ae21b63059bfdac90f33a4f314dcb7d8b7ee
Lu Stadler <lu@papill0n.org>
2018-05-27 13:27:23 +0200

    ~/.bin/rn: Run and notify if it runs "long"
    Where long is defined as "longer than one second" for now.  Might help
    with these media download thingies?  Also only notifies if the window is
    not focused. :)

---
+++ .bin/rn.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <cmd> [<args>*]\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	win := xGetActiveWindow()
+
+	cmd := exec.Command(os.Args[1], os.Args[2:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	start := time.Now()
+	err := cmd.Run()
+	duration := time.Since(start)
+
+	winAfter := xGetActiveWindow()
+
+	msg := fmt.Sprintf("Finished running %q in %s", cmdToString(cmd), duration)
+	if duration > 1*time.Second && win != winAfter {
+		notifyCompletion(msg)
+	}
+	fmt.Println(msg)
+
+	success := true
+	if err != nil {
+		success = false
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+	}
+
+	if !success {
+		os.Exit(1)
+	}
+}
+
+func notifyCompletion(msg string) {
+	notifyCmd := exec.Command("notify-send", msg)
+	notifyCmd.Run()
+}
+
+func xGetActiveWindow() string {
+	cmd := exec.Command("xdotool", "getactivewindow")
+	out, err := cmd.Output()
+	if err != nil {
+		return ""
+	}
+	return string(out)
+}
+
+func cmdToString(cmd *exec.Cmd) string {
+	return strings.Join(cmd.Args, " ")
+}
