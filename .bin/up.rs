ed2084a113ef9af58cbee46fa6eb7942e6e08580
Lu Stadler <lu@papill0n.org>
2018-06-29 08:36:23 +0200

    ~/.bin/up.rs: Rewrite for Rust 2018/nightly
    This was fairly frustrating, but it always seems to be.

--- .bin/up.rs
+++ .bin/up.rs
@@ -1,17 +1,29 @@
-use std::os;
+#![feature(rust_2018_preview)]
 
+use std::env;
+use std::path::PathBuf;
+use std::process;
+
+// Checks if it can find a directory (or file) up the directory tree.
+//
+// Can be used to check if you're in a vcs-versioned directory, like git, mercurial or svn.
+//
+// usage: up .git
+//        up .git <path-to-directory>
 fn main() {
-	let path = Path::new(os::args().get(1).as_slice());
-	let mut cwd = Path::new("");
-	while cwd != Path::new("/") {
-		cwd = os::getcwd();
-		if path.exists() {
-			println!("{}", cwd.display());
-			return;
-		} else {
-			os::change_dir(&Path::new(".."));
-		}
-	}
-
-	os::set_exit_status(1);
+    let repo_dir_name = &env::args().nth(1).expect("missing argument");
+    let mut path = env::args()
+        .nth(2)
+        .map(PathBuf::from)
+        .ok_or(0)
+        .or_else(|_| env::current_dir())
+        .unwrap();
+    while path.parent().is_some() {
+        if path.join(repo_dir_name).exists() {
+            println!("{}", path.canonicalize().unwrap().display());
+            return;
+        }
+        path = path.parent().unwrap().to_path_buf();
+    }
+    process::exit(1);
 }
