af374c9afdb7595270a1d1087cabf84affb0fb14
Lu Stadler <luc@spreadshirt.net>
2020-05-05 10:46:12 +0200

    ~/.config/dwm,~/.config/alacritty: Try out alacritty
    Configured to look like my previous st/sakura config, with fantasque as
    a font.  Light background as usual.  (That was kinda tricky, alacritty
    does not have pre-set themes, and the tango port on their wiki [1] is
    one with a dark background.)
    
    [1]: https://github.com/alacritty/alacritty/wiki/Color-schemes

--- .config/dwm/config.h
+++ .config/dwm/config.h
@@ -66,7 +66,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[] = { "rofi", "-show", "run", NULL };
-static const char *termcmd[]  = { "st", "-e", "tmux", NULL };
+static const char *termcmd[]  = { "alacritty", "-e", "tmux", NULL };
 
 static const char *fullscreenbrowsercmd[] = { "toggle-fullscreen.sh", NULL };
 
