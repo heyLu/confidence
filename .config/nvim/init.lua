d868e986651f2b5626f1d9eb8203cc54acfd8b69
Luna Stadler <lu@papill0n.org>
2022-01-25 20:43:58 +0100

    ~/.config/kanshi: Configure screen setup for new laptop`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,6 +2,9 @@
 vim.api.nvim_command('colorscheme peachpuff')
 vim.api.nvim_command('set number')
 
+-- highlight character that was jumped to correctly after lightspeed motions (https://github.com/ggandor/lightspeed.nvim/issues/66#issuecomment-952888479)
+vim.cmd[[highlight Cursor ctermfg=NONE ctermbg=NONE cterm=reverse]] 
+
 -- fast navigation with fzf
 vim.api.nvim_set_keymap('n', '<Leader>q', [[ <Cmd>b#<CR> ]], {noremap = true})
 vim.api.nvim_set_keymap('n', '<Leader>b', [[ <Cmd>Buffers<CR> ]], {noremap = true})
